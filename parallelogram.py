75fa178b74011131a0df3f4c895cc9fb2a8170fa
abc <abc@gmail.com>
2025-04-26 12:57:36 +0500

    Uploaded

---
+++ parallelogram.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the length of the base : "))
+height = float(input("Enter the hieght : "))
+area = base*height
+
+print(f"The area of the parallelogram is : {int(area)} square units")
