b257a84ed5e52269ac303afb2d89d807b244ce6d
abc <abc@gmail.com>
2025-04-29 12:28:19 +0500

    update

---
+++ src/complex_number.py
@@ -0,0 +1,5 @@
+real = float(input("Enter the real Part : "))
+imag = float(input("Enter the imaginary part : "))
+complex_number = complex(real,imag)
+
+print(f"The complex number is : {complex_number}")
