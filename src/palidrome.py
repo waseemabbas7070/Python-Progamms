c9c95bcc3c3357c4bd3885aebc7f34aef2b9d876
abc <abc@gmail.com>
2025-05-06 11:31:21 +0500

    Updated

---
+++ src/palidrome.py
@@ -0,0 +1,7 @@
+number = input("Enter the number : " )
+if number == number[::-1]:
+    print(f"{number} is the palidrine ")
+else:
+    print(f"{number} is the not palidrone ")
+# print(number)
+# print(number[::-1])
