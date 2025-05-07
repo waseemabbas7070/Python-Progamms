1999fbae5dc5940fb3b314e01323041acfdc6ddd
abc <abc@gmail.com>
2025-05-07 12:58:03 +0500

    updated

---
+++ src/random_number.py
@@ -0,0 +1,7 @@
+import random
+low = int(input("Enter the lower  bound : "))
+high = int(input("Enter the upper  bound : "))
+
+random_number = random.randint(low , high)
+
+print(f"Random number between {low} and {high} : {random_number}")
