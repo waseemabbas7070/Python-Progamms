df053f192befe02a7af4e01d1218886e066dae73
abc <abc@gmail.com>
2025-05-08 11:49:00 +0500

    UPdated

---
+++ src/anagram_string.py
@@ -0,0 +1,7 @@
+str1 = input("Enter  the first string : ")
+str2 = input("Enter the Second string : ")
+
+if sorted(str1) == sorted(str2):
+    print("Ther string are anagrams ")
+else:
+    print("The string are not anagrams ")    
