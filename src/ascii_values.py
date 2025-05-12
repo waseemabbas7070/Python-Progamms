9bdd879a8c12691d6b3edc814b7a1f940ad486f8
abc <abc@gmail.com>
2025-05-12 10:33:48 +0500

    Updated

---
+++ src/ascii_values.py
@@ -0,0 +1,4 @@
+char = input("Enter any  Character : ")
+acci_values = ord(char)
+
+print(f"The ASCII value of {char} is : {acci_values}")
---
+++ src/swap.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+
+# a,b = b,a
+c = b,a
+
+print(c)
