494fcdb8f6392265a527f5025fe372aca60587ff
abc <abc@gmail.com>
2025-05-12 10:41:37 +0500

    Updated

--- src/swap.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter the value of a : "))
-b = int(input("Enter the value of b : "))
-
-# a,b = b,a
-c = b,a
-
-print(c)
---
+++ src/swap_two_var.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+# a,b = b,a
+a,b = b,a
+
+print(f"After swaping : a = {a} , b = {b} ")
